5efda09ea672cb57cbbed9b6bb858c193ae4f5a2
anhld <wolfwithcode@gmail.com>
2021-03-25 16:26:33 +0700

    add graphql

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@nestjs/common';
+const axios = require('axios');
 
 @Injectable()
 export class JobsService {
-    private jobs = ["kitchen hand", "IT"];
-    getAllJobs(){
-        return this.jobs;
+  private jobs = ['kitchen hand', 'IT'];
+  async getAllJobs() {
+    try {
+      const data = JSON.stringify({
+        query: `
+                  query{
+                      applications{
+                      id
+                      createdAt
+                      updatedAt
+                      job
+                      {
+                          id
+                          title
+                          description
+                          phone
+                          salary
+                          salaryCategory{
+                          title
+                          }
+                      }
+                      employee{
+                          phone
+                          isVerified
+                          dealSalary
+                      }
+                      }
+                  }
+            `,
+        variables: {},
+      });
+
+      const config = {
+        method: 'post',
+        url: 'https://jobsrabbitstrapidev.herokuapp.com/graphql',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data: data,
+      };
+
+      const {
+        data: { data: applications },
+      } = await axios(config);
+      console.log(applications);
+      return applications;
+    } catch (error) {
+      console.log(error);
     }
+    
+  }
 }
