ba44b10ddef798fba761a06a737e8fe4f8976c48
anhld <wolfwithcode@gmail.com>
2021-03-25 15:53:18 +0700

    Add job controller and service

---
+++ src/jobs/jobs.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from '@nestjs/common';
+import { JobsService } from './jobs.service';
+
+@Controller('jobs')
+export class JobsController {
+    constructor(private jobsService: JobsService){
+        
+    }
+}
--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { JobsController } from './jobs.controller';
+import { JobsService } from './jobs.service';
 
-@Module({})
+@Module({
+  controllers: [JobsController],
+  providers: [JobsService]
+})
 export class JobsModule {}
