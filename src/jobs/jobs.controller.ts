2419396c462c6462cde700902ccfcd17297afbad
anhld <wolfwithcode@gmail.com>
2021-03-25 22:54:23 +0700

    Add create job api

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 
 @Controller('jobs')
@@ -8,7 +8,12 @@ export class JobsController {
   @Get()
   async getManyJobs(@Query() query) {
       const {start, limit} = query
-      console.log('param start, limit ', start, limit)
+    //   console.log('param start, limit ', start, limit)
     return this.jobsService.getManyJobs(start || 0, limit || 100);
+  }
+  @Post()
+  createJob(@Body() body){
+    console.log('body', body);
+    return this.jobsService.createJob(body);
   }
 }
--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -3,7 +3,49 @@ const axios = require('axios');
 
 @Injectable()
 export class JobsService {
-  private jobs = ['kitchen hand', 'IT'];
+//   private jobs = ['kitchen hand', 'IT'];
+  async createJob(jobInput){
+    console.log("jobInput", jobInput);
+    const query = `mutation{
+        createJob(input: {data: {title: ${JSON.stringify(jobInput.title || "unknown")}, email: ${JSON.stringify(jobInput.email)}, employer: ${JSON.stringify(jobInput.employer)}}}){
+          job{
+            id
+            title
+            employer{
+              id
+              shopName
+            }
+          }
+        }
+      }`
+        console.log("query ", query);
+    try {
+        const data = JSON.stringify({
+          query,
+          variables: {},
+        });
+  
+        const config = {
+          method: 'post',
+          url: 'https://jobsrabbitstrapidev.herokuapp.com/graphql',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          data: data,
+        };
+  
+        const {
+          data: { data: {createJob: {job}} },
+        } = await axios(config);
+
+        // const response = await axios(config);
+        // console.log("response ", response);
+        return {...job};
+      } catch (error) {
+        console.log(error);
+      }
+      
+  }
   async getManyJobs(start, limit) {
 
     try {
@@ -67,9 +109,9 @@ export class JobsService {
       const {
         data: { data: {jobs} },
       } = await axios(config);
-      console.log(jobs);
+    //   console.log(jobs);
       const {data: count } = await axios.get('https://jobsrabbitstrapidev.herokuapp.com/jobs/count')
-      console.log(count);
+    //   console.log(count);
       return {count, jobs};
     } catch (error) {
       console.log(error);
