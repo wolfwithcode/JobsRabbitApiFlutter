fcd513296967213324cfd74e21e9f468d6fc9119
anhld <wolfwithcode@gmail.com>
2021-03-26 12:14:49 +0700

    Add fragment graphql

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -1,74 +1,68 @@
 import { Injectable } from '@nestjs/common';
-import {ApolloClient, HttpLink, InMemoryCache} from 'apollo-boost'
-import ggl from 'graphql-tag'
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import gql from 'graphql-tag';
 import fetch from 'node-fetch';
 const axios = require('axios');
 
 const endpointURL = 'https://jobsrabbitstrapidev.herokuapp.com/graphql';
-const client = new ApolloClient(
-  {
-    link: new HttpLink({uri: endpointURL, fetch}),
-    cache: new InMemoryCache({addTypename: false})
-  }
-);
+const client = new ApolloClient({
+  link: new HttpLink({ uri: endpointURL, fetch }),
+  cache: new InMemoryCache({ addTypename: false }),
+});
+
+const jobDetailFragment = gql`
+                          fragment JobDetail on Job{
+                                                    id
+                                                        title
+                                                        employer{
+                                                          id
+                                                          shopName
+                                                        }
+                                                    
+                                                  }
+                        `;
 
 @Injectable()
 export class JobsService {
   //   private jobs = ['kitchen hand', 'IT'];
   async createJob(jobInput) {
     console.log('jobInput', jobInput);
-    const query = `mutation CreateJob($input: createJobInput){
+    const mutation = gql`
+    mutation CreateJob($input: createJobInput){
       createJob(input: $input){
         job{
-          id
-          title
-          employer{
-            id
-            shopName
-          }
+         ...JobDetail
         }
       }
-    }`;
+    }
+    ${jobDetailFragment}
+    `;
     const variables = {
       input: {
         data: {
-         ...jobInput 
+          ...jobInput,
         },
       },
     };
     // console.log("query ", query);
     try {
-      const data = JSON.stringify({
-        query,
-        variables,
-      });
-
-      const config = {
-        method: 'post',
-        url: 'https://jobsrabbitstrapidev.herokuapp.com/graphql',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        data: data,
-      };
-
       const {
         data: {
-          data: {
-            createJob: { job },
-          },
+          createJob: { job },
         },
-      } = await axios(config);
-
-      // const response = await axios(config);
-      // console.log("response ", response);
-      return { ...job };
+      } = await client.mutate({
+        mutation,
+        variables,
+        update: (cache, mutationResult) => {
+          console.log('mutationResult', mutationResult);
+        },
+      });
+      console.log('response ', job);
+      return job;
     } catch (error) {
       console.log(error);
     }
   }
-
-
 
   async getManyJobs(start, limit) {
     try {
@@ -128,7 +122,7 @@ export class JobsService {
         },
         data: data,
       };
-      const  query = ggl`{
+      const query = gql`{
         jobs( start: ${start} limit:${limit}  ){
         id
         state
@@ -168,13 +162,15 @@ export class JobsService {
         }
       }
     }`;
-      const {data: {jobs}} = await client.query({query})
-     
-        console.log(jobs);
+      const {
+        data: { jobs },
+      } = await client.query({ query });
+
+      console.log(jobs);
       const { data: count } = await axios.get(
         'https://jobsrabbitstrapidev.herokuapp.com/jobs/count',
       );
-        console.log(count);
+      console.log(count);
       return { count, jobs };
     } catch (error) {
       console.log(error);
