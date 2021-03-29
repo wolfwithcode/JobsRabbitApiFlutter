4217f2001dab56ceda5984ec58eddf6d39898559
anhld <wolfwithcode@gmail.com>
2021-03-29 18:26:41 +0700

    limit 1.000

--- src/jobs/jobs.service.ts
+++ src/jobs/jobs.service.ts
@@ -27,7 +27,7 @@ const indexFlexSearch = new FlexSearch({
   }
 });
 // console.log("docs", docs);
-indexFlexSearch.add(docs.slice(0,2000));
+indexFlexSearch.add(docs.slice(0,1000));
 
 // const start = async () => {
 //   await indexFlexSearch.add(docs);
