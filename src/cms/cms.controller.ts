996987fcf6211939e56caaf79232f84afee45c74
anhld <wolfwithcode@gmail.com>
2021-05-26 17:32:52 +0700

    get job by id

--- src/cms/cms.controller.ts
+++ src/cms/cms.controller.ts
@@ -1,14 +1,20 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { CmsService } from './cms.service';
 
 @Controller('cms')
 export class CmsController {
     constructor(private cmsService: CmsService) {}
   @Get('jobs')
-  async findJobs(@Query() query) {
+  findJobs(@Query() query) {
       console.log('query ', query)
     //   const {start, limit} = query
     //   console.log('param start, limit ', start, limit)
     return this.cmsService.findJobs(query);
+  }
+
+  @Get('jobs/:id')
+  findOneJob(@Param('id') id: string){
+    // console.log("id Param", id)
+    return this.cmsService.findOneJob(id);
   }
 }
--- src/cms/cms.service.ts
+++ src/cms/cms.service.ts
@@ -1,5 +1,14 @@
 import { HttpStatus, Injectable } from '@nestjs/common';
 import axios from 'axios';
+const URL = 'https://jobsrabbitstrapidev.herokuapp.com';
+
+enum MESSAGE {
+    SUCCESS = 'success',
+    FAILED = 'failed',
+    NOT_FOUND = 'not found',
+}
+
+
 @Injectable()
 export class CmsService {
   async findJobs(query) {
@@ -7,13 +16,13 @@ export class CmsService {
       console.log('query ', query);
       const { _start, _limit } = query;
       const { data: jobs } = await axios.get(
-        `https://jobsrabbitstrapidev.herokuapp.com/jobs?_start=${_start || 0}&_limit=${_limit || 100}`,
+        `${URL}/jobs?_start=${_start || 0}&_limit=${_limit || 100}`,
       );
 
       //   console.log('data', data);
       return {
         code: HttpStatus.OK,
-        message: HttpStatus.OK,
+        message: MESSAGE.SUCCESS,
         data: {
           total: jobs.length,
           data: jobs,
@@ -23,7 +32,32 @@ export class CmsService {
       console.log(error);
       return {
         code: HttpStatus.BAD_REQUEST,
-        message: JSON.stringify(error),
+        message: MESSAGE.NOT_FOUND,
+        data: {
+        },
+      };
+    }
+  }
+
+  async findOneJob(id) {
+    try {
+      console.log('id ', id);
+      
+      const { data: job } = await axios.get(
+        `${URL}/jobs/${id}`,
+      );
+
+      //   console.log('data', data);
+      return {
+        code: HttpStatus.OK,
+        message: MESSAGE.SUCCESS,
+        data: job
+      };
+    } catch (error) {
+      console.log(error);
+      return {
+        code: HttpStatus.BAD_REQUEST,
+        message: MESSAGE.NOT_FOUND,
         data: {
         },
       };
